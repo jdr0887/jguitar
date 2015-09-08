9cb321ffabcd3d24d649e1bdce0a2b8be7089bbe
Jason Reilly <jdr0887@gmail.com>
2015-09-08 11:50:22 -0400

    fixing data

--- jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/util/MIDINumber2NoteConverter.java
+++ jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/util/MIDINumber2NoteConverter.java
@@ -6,8 +6,8 @@ public class MIDINumber2NoteConverter {
             "B" };
 
     private static final Integer[][] data = new Integer[][] { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },
-            { 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, { 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 }, {},
-            { 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 }, { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, },
+            { 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, { 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 },
+            { 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 }, { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 },
             { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71 }, { 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83 },
             { 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95 },
             { 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107 },
@@ -31,6 +31,17 @@ public class MIDINumber2NoteConverter {
             for (int j = 0; j < data[i].length; j++) {
                 if (number.equals(data[i][j])) {
                     return i;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Integer getGuitarNote(Integer string, Integer note) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (string == i && note == j) {
+                    return data[i][j];
                 }
             }
         }

28bf30a282fb361b032ac1d8f3681146fef8bf68
Jason Reilly <jdr0887@gmail.com>
2015-09-17 10:18:02 -0400

    cleaning imports

--- jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/BaseDAO.java
+++ jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/BaseDAO.java
@@ -3,8 +3,6 @@ package com.kiluet.jguitar.dao;
 import java.io.Serializable;
 import java.util.List;
 
-import com.kiluet.jguitar.dao.model.Persistable;
-
 public interface BaseDAO<T extends Persistable, ID extends Serializable> {
 
     public abstract Long save(T entity) throws JGuitarDAOException;

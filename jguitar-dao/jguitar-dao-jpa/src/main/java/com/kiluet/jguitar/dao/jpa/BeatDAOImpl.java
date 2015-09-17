6a9588d8fdb5e6da4743c4ec1adeff1577f68817
Jason Reilly <jdr0887@gmail.com>
2015-09-17 16:01:39 -0400

    adding findBySongIdAndMeasureNumber

--- jguitar-dao/jguitar-dao-jpa/src/main/java/com/kiluet/jguitar/dao/jpa/BeatDAOImpl.java
+++ jguitar-dao/jguitar-dao-jpa/src/main/java/com/kiluet/jguitar/dao/jpa/BeatDAOImpl.java
@@ -1,8 +1,14 @@
 package com.kiluet.jguitar.dao.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +16,15 @@ import org.slf4j.LoggerFactory;
 import com.kiluet.jguitar.dao.BeatDAO;
 import com.kiluet.jguitar.dao.JGuitarDAOException;
 import com.kiluet.jguitar.dao.model.Beat;
+import com.kiluet.jguitar.dao.model.Beat_;
+import com.kiluet.jguitar.dao.model.Measure;
+import com.kiluet.jguitar.dao.model.Measure_;
+import com.kiluet.jguitar.dao.model.Song;
+import com.kiluet.jguitar.dao.model.Song_;
+import com.kiluet.jguitar.dao.model.Track;
+import com.kiluet.jguitar.dao.model.Track_;
 
-public class BeatDAOImpl extends BaseDAOImpl<Beat, Long> implements BeatDAO {
+public class BeatDAOImpl extends BaseDAOImpl<Beat, Long>implements BeatDAO {
 
     private final Logger logger = LoggerFactory.getLogger(BeatDAOImpl.class);
 
@@ -31,6 +44,28 @@ public class BeatDAOImpl extends BaseDAOImpl<Beat, Long> implements BeatDAO {
         query.setParameter("measureId", measureId);
         List<Beat> itemList = query.getResultList();
         return itemList;
+    }
+
+    @Override
+    public List<Beat> findBySongIdAndMeasureNumber(Long songId, Integer measureNumber) throws JGuitarDAOException {
+        logger.debug("ENTERING findBySongIdAndMeasureNumber(Long, Integer)");
+        CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Beat> crit = critBuilder.createQuery(Beat.class);
+        Root<Beat> root = crit.from(Beat.class);
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        Join<Beat, Measure> beatMeasureJoin = root.join(Beat_.measure);
+        predicates.add(critBuilder.equal(beatMeasureJoin.get(Measure_.number), measureNumber));
+
+        Join<Measure, Track> measureTrackJoin = beatMeasureJoin.join(Measure_.track);
+        Join<Track, Song> trackSongJoin = measureTrackJoin.join(Track_.song);
+        predicates.add(critBuilder.equal(trackSongJoin.get(Song_.id), songId));
+
+        crit.where(predicates.toArray(new Predicate[predicates.size()]));
+        // crit.groupBy(measureTrackJoin.get(Track_.number));
+        TypedQuery<Beat> query = getEntityManager().createQuery(crit);
+        List<Beat> ret = query.getResultList();
+        return ret;
     }
 
 }

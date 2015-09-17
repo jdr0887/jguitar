2c374b7ca4304eb9f652ff682c84811c361c81ad
Jason Reilly <jdr0887@gmail.com>
2015-09-17 16:03:28 -0400

    using ScheduledExecutorService

--- jguitar-desktop/src/main/java/com/kiluet/jguitar/player/SongPlayer.java
+++ jguitar-desktop/src/main/java/com/kiluet/jguitar/player/SongPlayer.java
@@ -1,11 +1,15 @@
 package com.kiluet.jguitar.player;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import com.kiluet.jguitar.dao.model.Beat;
 import com.kiluet.jguitar.dao.model.InstrumentString;
 import com.kiluet.jguitar.dao.model.Measure;
+import com.kiluet.jguitar.dao.model.Song;
 import com.kiluet.jguitar.dao.model.Track;
 import com.kiluet.jguitar.desktop.components.MeasurePane;
 import com.kiluet.jguitar.desktop.components.NoteTextField;
@@ -23,15 +28,17 @@ public class SongPlayer {
 
     private static final Logger logger = LoggerFactory.getLogger(SongPlayer.class);
 
+    private final Map<String, List<Beat>> beatMap = new LinkedHashMap<String, List<Beat>>();
+
     private SongPane songPane;
 
-    private Beat previousBeat;
+    private ScheduledExecutorService es;
 
-    private Beat currentBeat;
+    private List<Beat> previousBeats;
 
-    private Timer timer = null;
+    private List<Beat> currentBeats;
 
-    private Iterator<Beat> beatIter;
+    private Iterator<List<Beat>> beatIter;
 
     private SongPlayerState songPlayerState = SongPlayerState.PAUSED;
 
@@ -42,94 +49,103 @@ public class SongPlayer {
     }
 
     private void init() {
-        LinkedList<Beat> beatList = new LinkedList<Beat>();
-        List<Track> tracks = songPane.getSong().getTracks();
-        for (Track track : tracks) {
-            List<Measure> measures = track.getMeasures();
-            for (Measure measure : measures) {
-                List<Beat> beats = measure.getBeats();
-                for (Beat beat : beats) {
-                    beatList.add(beat);
+        Song song = songPane.getSong();
+        for (Track track : song.getTracks()) {
+            for (Measure measure : track.getMeasures()) {
+                for (Beat beat : measure.getBeats()) {
+                    String key = String.format("%d_%d", measure.getNumber(), beat.getNumber());
+                    if (beatMap.get(key) == null) {
+                        beatMap.put(key, new LinkedList<Beat>());
+                    }
+                    beatMap.get(key).add(beat);
                 }
             }
         }
-        this.beatIter = beatList.iterator();
+        this.beatIter = beatMap.values().iterator();
+        currentBeats = beatIter.next();
     }
 
     public void play() {
         logger.info("ENTERING play()");
-        this.songPlayerState = SongPlayerState.PLAYING;
-        this.timer = new Timer();
+        this.es = Executors.newSingleThreadScheduledExecutor();
 
-        this.timer.scheduleAtFixedRate(new TimerTask() {
+        if (!beatIter.hasNext()) {
+            this.beatIter = beatMap.values().iterator();
+        }
+
+        this.songPlayerState = SongPlayerState.PLAYING;
+        es.scheduleAtFixedRate(new Runnable() {
 
             @Override
             public void run() {
                 logger.info("ENTERING run()");
                 try {
 
-                    if (previousBeat != null) {
+                    if (CollectionUtils.isNotEmpty(previousBeats)) {
 
-                        TrackPane trackPane = (TrackPane) songPane
-                                .lookup(String.format("#TrackPane_%d", previousBeat.getMeasure().getTrack().getId()));
-                        MeasurePane measurePane = (MeasurePane) trackPane
-                                .lookup(String.format("#MeasurePane_%d", previousBeat.getMeasure().getId()));
-                        List<InstrumentString> instrumentStrings = previousBeat.getMeasure().getTrack().getInstrument()
-                                .getStrings();
-                        for (InstrumentString instrumentString : instrumentStrings) {
-                            NoteTextField noteTextField = (NoteTextField) measurePane.lookup(
-                                    String.format("#NoteTextField_%d_%d_%d", previousBeat.getMeasure().getNumber(),
-                                            previousBeat.getNumber(), instrumentString.getString()));
-                            if (noteTextField != null && StringUtils.isNotEmpty(noteTextField.getText())) {
-                                noteTextField.colorBlack();
+                        for (Beat beat : previousBeats) {
+                            TrackPane trackPane = (TrackPane) songPane
+                                    .lookup(String.format("#TrackPane_%d", beat.getMeasure().getTrack().getId()));
+                            MeasurePane measurePane = (MeasurePane) trackPane
+                                    .lookup(String.format("#MeasurePane_%d", beat.getMeasure().getId()));
+                            List<InstrumentString> instrumentStrings = beat.getMeasure().getTrack().getInstrument()
+                                    .getStrings();
+                            for (InstrumentString instrumentString : instrumentStrings) {
+                                NoteTextField noteTextField = (NoteTextField) measurePane.lookup(String.format(
+                                        "#NoteTextField_%d_%d_%d_%d", beat.getMeasure().getTrack().getId(),
+                                        beat.getMeasure().getNumber(), beat.getNumber(), instrumentString.getString()));
+                                if (noteTextField != null && StringUtils.isNotEmpty(noteTextField.getText())) {
+                                    noteTextField.colorBlack();
+                                }
                             }
                         }
 
                     }
 
-                    if (currentBeat == null) {
-                        currentBeat = beatIter.next();
-                    }
-
-                    TrackPane trackPane = (TrackPane) songPane
-                            .lookup(String.format("#TrackPane_%d", currentBeat.getMeasure().getTrack().getId()));
-                    MeasurePane measurePane = (MeasurePane) trackPane
-                            .lookup(String.format("#MeasurePane_%d", currentBeat.getMeasure().getId()));
-                    List<InstrumentString> instrumentStrings = currentBeat.getMeasure().getTrack().getInstrument()
-                            .getStrings();
-                    for (InstrumentString instrumentString : instrumentStrings) {
-                        NoteTextField noteTextField = (NoteTextField) measurePane
-                                .lookup(String.format("#NoteTextField_%d_%d_%d", currentBeat.getMeasure().getNumber(),
-                                        currentBeat.getNumber(), instrumentString.getString()));
-                        if (noteTextField != null && StringUtils.isNotEmpty(noteTextField.getText())) {
-                            noteTextField.colorRed();
-                        }
-                    }
-
                     if (!beatIter.hasNext()) {
-                        timer.cancel();
+                        pause();
                         return;
                     }
 
-                    previousBeat = currentBeat;
-                    currentBeat = beatIter.next();
+                    for (Beat beat : currentBeats) {
+
+                        TrackPane trackPane = (TrackPane) songPane
+                                .lookup(String.format("#TrackPane_%d", beat.getMeasure().getTrack().getId()));
+                        MeasurePane measurePane = (MeasurePane) trackPane
+                                .lookup(String.format("#MeasurePane_%d", beat.getMeasure().getId()));
+                        List<InstrumentString> instrumentStrings = beat.getMeasure().getTrack().getInstrument()
+                                .getStrings();
+                        for (InstrumentString instrumentString : instrumentStrings) {
+                            NoteTextField noteTextField = (NoteTextField) measurePane.lookup(String.format(
+                                    "#NoteTextField_%d_%d_%d_%d", beat.getMeasure().getTrack().getId(),
+                                    beat.getMeasure().getNumber(), beat.getNumber(), instrumentString.getString()));
+                            if (noteTextField != null && StringUtils.isNotEmpty(noteTextField.getText())) {
+                                noteTextField.colorRed();
+                            }
+                        }
+
+                    }
+
+                    previousBeats = currentBeats;
+                    currentBeats = beatIter.next();
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
 
             }
-        }, 0, 1000);
 
+        }, 0, 1, TimeUnit.SECONDS);
     }
 
     public void pause() {
         logger.info("ENTERING pause()");
-        this.timer.cancel();
         this.songPlayerState = SongPlayerState.PAUSED;
+        this.es.shutdownNow();
     }
 
     public boolean isPlaying() {
+
         return this.songPlayerState == SongPlayerState.PLAYING;
     }
 

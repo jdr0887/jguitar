9d1fca59277ff656a3339b385844a78098ddf004
Jason Reilly <jdr0887@gmail.com>
2015-09-28 10:45:26 -0400

    adding measure index and close repeat count

--- jguitar-desktop/src/main/java/com/kiluet/jguitar/desktop/components/MeasureHeaderPane.java
+++ jguitar-desktop/src/main/java/com/kiluet/jguitar/desktop/components/MeasureHeaderPane.java
@@ -2,15 +2,14 @@ package com.kiluet.jguitar.desktop.components;
 
 import com.kiluet.jguitar.dao.model.Measure;
 
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class MeasureHeaderPane extends GridPane {
 
@@ -19,55 +18,27 @@ public class MeasureHeaderPane extends GridPane {
     public MeasureHeaderPane(Measure measure) {
         super();
         this.measure = measure;
+        setId(String.format("MeasureHeaderPane_%d", measure.getId()));
         init();
     }
 
-    private void init() {
+    public void init() {
+        // setStyle(" -fx-border-color: red;");
 
-        int stringCount = measure.getTrack().getInstrument().getStrings().size();
-        for (int i = 0; i < stringCount; i++) {
+        Text measureIndexText = new Text(measure.getNumber().toString());
+        measureIndexText.setFill(Color.RED);
+        measureIndexText.setFont(Font.font(java.awt.Font.SANS_SERIF, FontWeight.THIN, 9));
+        add(measureIndexText, 0, 0);
+        GridPane.setHalignment(measureIndexText, HPos.LEFT);
+        GridPane.setMargin(measureIndexText, new Insets(0, 0, 0, 6));
+        GridPane.setHgrow(measureIndexText, Priority.ALWAYS);
 
-            StackPane stackPane = new StackPane();
-            // -fx-border-color: red;
-            stackPane.setMinHeight(18);
-            stackPane.setMaxHeight(18);
-            stackPane.setStyle("-fx-padding: 0;");
-
-            Line line = new Line(0, i + 1 * 10, 15, i + 1 * 10);
-            line.setStroke(Color.LIGHTGRAY);
-
-            stackPane.getChildren().addAll(line);
-
-            add(stackPane, 3, i + 1);
-
-        }
-
-        Line a = new Line(0, 0, 0, (stringCount - 1) * 18 - 3);
-        a.setStrokeWidth(4);
-        add(a, 0, 0, 1, 7);
-
-        Line b = new Line(5, 0, 5, (stringCount - 1) * 18);
-        b.setStrokeWidth(1);
-        add(b, 1, 0, 3, 7);
-        GridPane.setMargin(b, new Insets(0, 0, 0, 2));
-
-        if (measure.getOpenRepeat()) {
-            GridPane circleGrid = new GridPane();
-            circleGrid.setAlignment(Pos.CENTER);
-            // circleGrid.setStyle("-fx-border-color: red;");
-
-            Circle firstCircle = new Circle(0, 0, 3);
-            circleGrid.add(firstCircle, 0, 0);
-            GridPane.setVgrow(firstCircle, Priority.ALWAYS);
-            GridPane.setValignment(firstCircle, VPos.CENTER);
-
-            Circle secondCircle = new Circle(0, 0, 3);
-            circleGrid.add(secondCircle, 0, 1);
-            GridPane.setVgrow(secondCircle, Priority.ALWAYS);
-            GridPane.setValignment(secondCircle, VPos.CENTER);
-
-            add(circleGrid, 2, 0, 3, 7);
-            GridPane.setMargin(circleGrid, new Insets(0, 0, 0, 3));
+        if (measure.getCloseRepeat() != null) {
+            Text closeRepeatText = new Text(String.format("x%d", measure.getCloseRepeat()));
+            closeRepeatText.setFont(Font.font(java.awt.Font.SANS_SERIF, FontWeight.THIN, 9));
+            add(closeRepeatText, 1, 0);
+            GridPane.setHalignment(closeRepeatText, HPos.RIGHT);
+            GridPane.setMargin(closeRepeatText, new Insets(0, 6, 0, 0));
         }
 
     }

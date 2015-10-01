7462095ef9d360d45a7b02468db0e37eea4ab299
Jason Reilly <jdr0887@gmail.com>
2015-10-01 16:12:01 -0400

    adding findAll and findByProgram

--- jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/InstrumentDAO.java
+++ jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/InstrumentDAO.java
@@ -6,6 +6,10 @@ import com.kiluet.jguitar.dao.model.Instrument;
 
 public interface InstrumentDAO extends BaseDAO<Instrument, Long> {
 
+    public abstract List<Instrument> findAll() throws JGuitarDAOException;
+
     public abstract List<Instrument> findByName(String name) throws JGuitarDAOException;
+
+    public abstract Instrument findByProgram(Integer program) throws JGuitarDAOException;
 
 }

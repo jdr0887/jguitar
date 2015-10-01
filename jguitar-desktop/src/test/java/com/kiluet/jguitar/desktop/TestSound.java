be00dfedef785fa1310a8ecb548c4fe051c54797
Jason Reilly <jdr0887@gmail.com>
2015-10-01 16:17:53 -0400

    messing around

--- jguitar-desktop/src/test/java/com/kiluet/jguitar/desktop/TestSound.java
+++ jguitar-desktop/src/test/java/com/kiluet/jguitar/desktop/TestSound.java
@@ -1,17 +1,18 @@
 package com.kiluet.jguitar.desktop;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 
 import javax.sound.midi.Instrument;
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Soundbank;
 import javax.sound.midi.Synthesizer;
 
 import org.junit.Test;
-
-import com.kiluet.jguitar.dao.model.InstrumentString;
 
 public class TestSound {
 
@@ -20,17 +21,40 @@ public class TestSound {
         try {
             Synthesizer synthesizer = MidiSystem.getSynthesizer();
             synthesizer.open();
+            // Soundbank soundbank = synthesizer.getDefaultSoundbank();
+            // System.out.println(soundbank.getName());
+            // System.out.println(soundbank.getDescription());
             Instrument[] instruments = synthesizer.getAvailableInstruments();
             for (Instrument instrument : instruments) {
-                System.out.println(instrument.toString());
-            }
+                if (instrument.getPatch().getBank() == 0) {
+                    System.out.println(instrument.toString());
+                }
 
+                // System.out.println("instrument.getName(): " + instrument.getName());
+                // System.out.println("instrument.getClass().getName(): " + instrument.getClass().getName());
+                // System.out.println("instrument.getPatch().getBank(): " + instrument.getPatch().getBank());
+                // System.out.println("instrument.getPatch().getProgram(): " + instrument.getPatch().getProgram());
+            }
             synthesizer.close();
 
         } catch (MidiUnavailableException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void showDeluxeInstruments() {
+        try {
+            Soundbank soundbank = MidiSystem.getSoundbank(new File("/home/jdr0887/Downloads", "soundbank-deluxe.gm"));
+            // Soundbank soundbank = MidiSystem
+            // .getSoundbank(new File("/home/jdr0887/Downloads/fluid-soundfont", "FluidR3 GM2-2.SF2"));
+            Instrument[] instruments = soundbank.getInstruments();
+            for (Instrument instrument : instruments) {
+                System.out.println(instrument.toString());
+            }
+        } catch (InvalidMidiDataException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -53,53 +77,54 @@ public class TestSound {
             Synthesizer synthesizer = MidiSystem.getSynthesizer();
             synthesizer.open();
             Instrument[] instruments = synthesizer.getAvailableInstruments();
-            for (Instrument i : instruments) {
-                System.out.println(i.toString());
-            }
 
             MidiChannel[] channels = synthesizer.getChannels();
 
             try {
 
                 int idx = 27;
-                System.out.println(instruments[idx].toString());
-                channels[0].programChange(0, idx);
-
-                for (Integer note : Arrays.asList(40, 45, 50, 55, 59, 64)) {
-                    channels[0].noteOn(note, 200);
-                    Thread.sleep(2000);
-                    channels[0].noteOff(note);
-                }
+                // System.out.println(instruments[idx].toString());
+                // channels[0].programChange(0, idx);
+                //
+                // for (Integer note : Arrays.asList(40, 45, 50, 55, 59, 64)) {
+                // channels[0].noteOn(note, 200);
+                // Thread.sleep(2000);
+                // channels[0].noteOff(note);
+                // }
 
                 idx = 34;
-                System.out.println(instruments[idx].toString());
-                channels[0].programChange(0, idx);
-
-                for (Integer note : Arrays.asList(28, 33, 38, 42)) {
-                    channels[0].noteOn(note, 200);
-                    Thread.sleep(2000);
-                    channels[0].noteOff(note);
-                }
+                // System.out.println(instruments[idx].toString());
+                // channels[0].programChange(0, idx);
+                //
+                // for (Integer note : Arrays.asList(28, 33, 38, 42)) {
+                // channels[0].noteOn(note, 200);
+                // Thread.sleep(2000);
+                // channels[0].noteOff(note);
+                // }
 
                 idx = 42;
                 System.out.println(instruments[idx].toString());
                 channels[0].programChange(0, idx);
 
-                for (Integer note : Arrays.asList(36, 43, 50, 57)) {
-                    channels[0].noteOn(note, 200);
-                    Thread.sleep(2000);
-                    channels[0].noteOff(note);
-                }
+                // for (Integer note : Arrays.asList(36, 43, 50, 57)) {
+                // channels[0].noteOn(note, 200);
+                // Thread.sleep(2000);
+                // channels[0].noteOff(note);
+                // }
 
                 idx = 40;
-                System.out.println(instruments[idx].toString());
-                channels[0].programChange(0, idx);
+                // System.out.println(instruments[idx].toString());
+                // channels[0].programChange(0, idx);
+                //
+                // for (Integer note : Arrays.asList(55, 62, 69, 76)) {
+                // channels[0].noteOn(note, 200);
+                // Thread.sleep(2000);
+                // channels[0].noteOff(note);
+                // }
 
-                for (Integer note : Arrays.asList(55, 62, 69, 76)) {
-                    channels[0].noteOn(note, 200);
-                    Thread.sleep(2000);
-                    channels[0].noteOff(note);
-                }
+                channels[0].noteOn(62, 200);
+                Thread.sleep(3000);
+                channels[0].noteOff(62);
 
             } catch (InterruptedException e) {
                 e.printStackTrace();

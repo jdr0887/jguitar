3f674d24ffbb02dd4b1449880e2845f145fc3ac6
Jason Reilly <jdr0887@gmail.com>
2015-10-01 16:12:08 -0400

    adding findAll and findByProgram

--- jguitar-dao/jguitar-dao-jpa/src/main/java/com/kiluet/jguitar/dao/jpa/InstrumentDAOImpl.java
+++ jguitar-dao/jguitar-dao-jpa/src/main/java/com/kiluet/jguitar/dao/jpa/InstrumentDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.TypedQuery;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ import com.kiluet.jguitar.dao.InstrumentDAO;
 import com.kiluet.jguitar.dao.JGuitarDAOException;
 import com.kiluet.jguitar.dao.model.Instrument;
 
-public class InstrumentDAOImpl extends BaseDAOImpl<Instrument, Long> implements InstrumentDAO {
+public class InstrumentDAOImpl extends BaseDAOImpl<Instrument, Long>implements InstrumentDAO {
 
     private final Logger logger = LoggerFactory.getLogger(InstrumentDAOImpl.class);
 
@@ -25,12 +26,33 @@ public class InstrumentDAOImpl extends BaseDAOImpl<Instrument, Long> implements 
     }
 
     @Override
+    public List<Instrument> findAll() throws JGuitarDAOException {
+        logger.debug("ENTERING findAll()");
+        TypedQuery<Instrument> query = getEntityManager().createNamedQuery("Instrument.findAll", Instrument.class);
+        List<Instrument> itemList = query.getResultList();
+        return itemList;
+    }
+
+    @Override
     public List<Instrument> findByName(String name) throws JGuitarDAOException {
         logger.debug("ENTERING findByName(String)");
         TypedQuery<Instrument> query = getEntityManager().createNamedQuery("Instrument.findByName", Instrument.class);
         query.setParameter("name", name);
         List<Instrument> itemList = query.getResultList();
         return itemList;
+    }
+
+    @Override
+    public Instrument findByProgram(Integer program) throws JGuitarDAOException {
+        logger.debug("ENTERING findByProgram(Integer)");
+        TypedQuery<Instrument> query = getEntityManager().createNamedQuery("Instrument.findByProgram",
+                Instrument.class);
+        query.setParameter("program", program);
+        List<Instrument> itemList = query.getResultList();
+        if (CollectionUtils.isNotEmpty(itemList)) {
+            return itemList.get(0);
+        }
+        return null;
     }
 
 }

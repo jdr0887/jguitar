48f6b99e18463aaa0840032da52cf6c19f296ee6
Jason Reilly <jdr0887@gmail.com>
2015-10-01 16:16:12 -0400

    cleaning

--- jguitar-desktop/src/main/java/com/kiluet/jguitar/desktop/Main.java
+++ jguitar-desktop/src/main/java/com/kiluet/jguitar/desktop/Main.java
@@ -8,10 +8,8 @@ import com.kiluet.jguitar.config.Config;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 

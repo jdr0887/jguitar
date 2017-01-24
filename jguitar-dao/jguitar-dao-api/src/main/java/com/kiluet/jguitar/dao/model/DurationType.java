56461d738e263c0355fb3b751d389f0ad5e581f2
Jason Reilly <jdr0887@gmail.com>
2017-01-24 15:56:39 -0500

    adding length

--- jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/model/DurationType.java
+++ jguitar-dao/jguitar-dao-api/src/main/java/com/kiluet/jguitar/dao/model/DurationType.java
@@ -2,32 +2,43 @@ package com.kiluet.jguitar.dao.model;
 
 public enum DurationType {
 
-    WHOLE(1),
+   WHOLE(1, 64),
 
-    HALF(2),
+   HALF(2, 32),
 
-    QUARTER(4),
+   QUARTER(4, 16),
 
-    EIGHTH(8),
+   EIGHTH(8, 8),
 
-    SIXTEENTH(16),
+   SIXTEENTH(16, 4),
 
-    THIRTY_SECOND(32),
+   THIRTY_SECOND(32, 2),
 
-    SIXTY_FOURTH(64);
+   SIXTY_FOURTH(64, 1);
 
-    private Integer code;
+   private Integer code;
 
-    private DurationType(Integer code) {
-        this.code = code;
-    }
+   private Integer length;
 
-    public Integer getCode() {
-        return code;
-    }
+   private DurationType(Integer code, Integer length) {
+      this.code = code;
+      this.length = length;
+   }
 
-    public void setCode(Integer code) {
-        this.code = code;
-    }
+   public Integer getCode() {
+      return code;
+   }
+
+   public void setCode(Integer code) {
+      this.code = code;
+   }
+
+   public Integer getLength() {
+      return length;
+   }
+
+   public void setLength(Integer length) {
+      this.length = length;
+   }
 
 }

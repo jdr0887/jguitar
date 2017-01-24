60a16d6062dda7abe73ec57040ae9378bc21c837
Jason Reilly <jdr0887@gmail.com>
2017-01-24 15:55:31 -0500

    initial commit

---
+++ jguitar-desktop/src/test/java/com/kiluet/jguitar/desktop/EmptyBeatTest.java
@@ -0,0 +1,73 @@
+package com.kiluet.jguitar.desktop;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class EmptyBeatTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(20, 20, 20, 20));
+
+        HBox hbox = new HBox();
+        int stringCount = 6;
+        hbox.getChildren().add(createMeasurePane(stringCount));
+        pane.setCenter(hbox);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(100);
+        primaryStage.setHeight(200);
+        primaryStage.show();
+    }
+
+    public GridPane createMeasurePane(int stringCount) {
+
+        GridPane gridPane = new GridPane();
+
+        for (int i = 0; i < stringCount; i++) {
+
+            StackPane stackPane = new StackPane();
+            // -fx-border-color: red;
+            stackPane.setMinHeight(18);
+            stackPane.setMaxHeight(18);
+            stackPane.setStyle("-fx-padding: 0;");
+
+            Line line = new Line(0, i + 1 * 10, 40, i + 1 * 10);
+            line.setStroke(Color.LIGHTGRAY);
+
+            stackPane.getChildren().addAll(line);
+
+            gridPane.add(stackPane, 0, i + 1);
+
+        }
+
+        GridPane circleGrid = new GridPane();
+        circleGrid.setAlignment(Pos.CENTER);
+        Rectangle rectangle = new Rectangle(13, 5);
+        circleGrid.add(rectangle, 0, 0);
+        GridPane.setMargin(rectangle, new Insets(0, 0, 12, 0));
+        gridPane.add(circleGrid, 0, 0, 2, 7);
+
+        return gridPane;
+    }
+
+    public static void main(String[] args) {
+        EmptyBeatTest.launch(EmptyBeatTest.class, args);
+    }
+}
